6e2eb85d6d55964a14e4216ad8db337955407b79
bageus <36338626+bageus@users.noreply.github.com>
2026-03-14 20:17:03 +0300

    Update rateLimiter.test.js

--- test/rateLimiter.test.js
+++ test/rateLimiter.test.js
@@ -12,7 +12,7 @@ function mockReq({ forwarded, ip, remoteAddress } = {}) {
 
 test('parseClientIp should use req.ip even if x-forwarded-for exists', () => {
   const ip = parseClientIp(mockReq({ forwarded: '203.0.113.1, 10.0.0.2', ip: '127.0.0.1' }));
-  assert.equal(ip, '203.0.113.1');
+  assert.equal(ip, '127.0.0.1');
 });
 
 test('parseClientIp should fallback to req.ip', () => {
